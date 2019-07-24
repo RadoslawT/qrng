7152209c2901cb72d3b63b3b59018cd15885a40b
radoslawt <radoslawtomczynski@gmail.com>
2019-07-24 17:32:06 +0200

    Clean up

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include <simple2d.h>
-#include <random>
-#include <iostream>
-#include <string>
 #include "qrng.h"
 
 S2D_Window *window;
-
-std::random_device dev;
-std::mt19937 rng(dev());
-std::uniform_int_distribution<std::mt19937::result_type> dist6(1,10);
 
 int windowWidth = 640;
 int windowHeight = 480;
@@ -17,9 +10,6 @@ int screenHeight;
 
 
 float bgValue = 0.0;
-float bgRed   = 0.0;
-float bgGreen = 0.0;
-float bgBlue  = 0.0;
 
 void render();
 void update();
@@ -68,47 +58,20 @@ void render(){
 
 void update(){
   if (window->frames % 5 == 0){
-    int   dist = dist6(rng);
-    float similarity = dist / 10.0;
-    std::string number;
-    getRandomNumber(6);
+    float similarity;
+    similarity = getRandomNumber(6);
     bgValue = similarity;
 
     printf("%f\n", bgValue);
-    printf("%i\n", dist);
   }
 }
 
 void onKey(S2D_Event e) {
   switch(e.type){
     case S2D_KEY_DOWN:
-      if (strcmp(e.key, "Up") == 0)
-        bgLighter();
-      if (strcmp(e.key, "Down") == 0)
-        bgDarker();
       if (strcmp(e.key, "Escape") == 0){
         S2D_Close(window);
       }
       break;
   }
-}
-
-void bgDarker(){
-  if(bgValue < 0.1){
-    return;
-  }
-  puts("darker");
-  printf("\nbgValue: %f", bgValue);
-  bgValue -= 0.1;
-  bgRed = bgGreen = bgBlue = bgValue;
-}
-
-void bgLighter(){
-  if(bgValue > 0.9){
-    return;
-  }
-  puts("lighter");
-  printf("\nbgValue: %f", bgValue);
-  bgValue += 0.1;
-  bgRed = bgGreen = bgBlue = bgValue;
 }
